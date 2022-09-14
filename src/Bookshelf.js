2ac14d62b6c3517810ee0ab238efda7d239eb162
Bryan Cruz <bryan.c@ens-labs.com>
2022-09-13 21:14:06 -0400

    bookshelf component renders out bookshelf name

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,18 +3,34 @@ import Book from './Book';
 
 class Bookshelf extends Component {
     
+    getBookshelfName(title){
+        switch(title){
+            case 'currentlyReading':
+                return 'Currently Reading';
+            case 'wantToRead':
+                return 'Want to Read';
+            case 'read':
+                return 'Read';
+            default: 
+                return title;
+        }
+    }
     
     render() {
         const { title, books } =  this.props;
 
         return (
             <div className="bookshelf">
-                <h2 className="bookshelf-title">{title}</h2>
+                <h2 className="bookshelf-title">{this.getBookshelfName(title)}</h2>
                 <div className="bookshelf-books">
                     <ol className="books-grid">
                         {
                             books.map((book) => {
-                                <Book book={book}/>
+                                return(
+                                <li key={book.id}>
+                                    <Book book={book}/>
+                                </li>
+                                )
                             })
                         }
                     </ol>
